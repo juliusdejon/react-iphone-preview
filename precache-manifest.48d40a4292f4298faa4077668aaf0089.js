7d874e4408bedc324cfb2d9c5ec43470bcd14de6
Julius Dejon <zjmdejon@globe.com.ph>
2020-05-18 15:37:35 +0800

    Updates

---
+++ precache-manifest.48d40a4292f4298faa4077668aaf0089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8eaa0885cad310243f192a5058904e4",
+    "url": "/react-iphone/index.html"
+  },
+  {
+    "revision": "120970527c7ace4eecfd",
+    "url": "/react-iphone/static/css/main.82a958a4.chunk.css"
+  },
+  {
+    "revision": "dfcb117a96f692a350fc",
+    "url": "/react-iphone/static/js/2.4535283c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-iphone/static/js/2.4535283c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120970527c7ace4eecfd",
+    "url": "/react-iphone/static/js/main.959c16f0.chunk.js"
+  },
+  {
+    "revision": "0db0f55a3e9d1860be36",
+    "url": "/react-iphone/static/js/runtime-main.debd3022.js"
+  }
+]);
